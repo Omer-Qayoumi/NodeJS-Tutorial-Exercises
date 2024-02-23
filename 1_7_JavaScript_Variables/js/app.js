672c249c82cde7a8d7bec775a077fa1c7d8a0bbc
Omer Qayoumi <omerqayoumi1914@gmail.com>
2024-02-24 03:06:51 +0430

    vaiables and datatypes directory are added

---
+++ 1_7_JavaScript_Variables/js/app.js
@@ -0,0 +1,20 @@
+// 1-6 video start
+var age = 23;
+var score = 100;
+var userName = "عمر";
+var courseName = "آموزش حرفه یی گیت و گیت هاب";
+console.log(userName);
+console.log(courseName);
+console.log(typeof userName);
+console.log(typeof courseName);
+console.log(typeof age);
+// 1-6 video end
+// video number 7 start
+let number1 = 3;
+let number2 = 2;
+console.log(number1 + number2); // 5
+console.log(number1 - number2); // 1
+console.log(number1 * number2); // 6
+console.log(number1 / number2); // 1.5
+console.log(number1 ** number2); // 9
+// video number 7 start
---
+++ 8_14_JavaScript_Data-Types/js/app.js
@@ -0,0 +1,76 @@
+// 8 video start
+var ahmadUserName = "Ahamd";
+var omerUserName = "Omer";
+var shillaUserName = "Shilla";
+console.log(ahmadUserName - omerUserName);
+console.log(ahmadUserName * omerUserName);
+console.log(ahmadUserName / omerUserName);
+console.log(ahmadUserName + omerUserName);
+console.log(ahmadUserName + " " + omerUserName + " " + shillaUserName);
+// 8 video end
+// 9 video start
+var userName = "omer";
+console.log(userName + 10);
+//
+var userName = "omer";
+var score = 100;
+console.log(userName + score);
+//
+var userName = "omer";
+var score = "100";
+console.log(userName + score);
+//
+var userName = "50";
+var score = "100";
+console.log(userName + score);
+//
+var userName = +"50";
+var score = +"100";
+console.log(userName + score);
+//
+var userName = Number("200");
+var score = Number("30");
+console.log(userName + score);
+//
+var num = "Shilla";
+console.log(Number(num));
+//
+var num = +"Shilla";
+console.log(Number(num));
+// 9 video end
+// 10 video start
+var age = String("20");
+console.log(typeof age);
+//
+var age = 40 + " ";
+console.log(typeof age);
+// 10 video ended
+// 11 video start Prompt
+var age = prompt("لطفا سن خود را وارد نمایید");
+console.log(age);
+//
+var userName = prompt("لطفا نام خود را وارد نمایید");
+console.log(userName);
+//
+var lastName = prompt("لطفا تخلص خود را وارد نمایید");
+console.log(lastName);
+//11 video ended
+//12 video start Prompt
+var userBirthDate = prompt("لطفا سال تولد خود را وارد کنید");
+var userAge = 1402 - userBirthDate;
+console.log(userAge);
+//12 video ended
+//13 video start Prompt
+var number1 = Number(prompt("عدد اول را وارد کنید"));
+var number2 = Number(prompt("عدد دوم را وارد کنید"));
+console.log(typeof number1);
+console.log(typeof number2);
+var sum = number1 + number2;
+console.log(sum);
+//13 video ended
+// 14 video start Boolian
+var isLogin = true;
+var isAdmin = false;
+console.log(typeof isLogin);
+console.log(typeof isAdmin);
+// 14 video ended
